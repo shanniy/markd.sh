dd3807b5e617a8f3833a7680c06bdd88cf975319
shanniy <mason.owen36+git@gmail.com>
2021-03-01 16:26:00 +0000

    Added -h option

---
+++ README.md
@@ -0,0 +1,3 @@
+# What is this?
+A shell script that create 'bookmarks' for directories.
+Mostly a project for me to learn some shell scripting.
--- markd.sh
+++ markd.sh
@@ -10,7 +10,7 @@ while getopts ":acdhlp:rv" opt; do
             ;;
         d ) echo "TODO: Delete" ; exit 2
             ;;
-        h ) echo "TODO: Help" ; exit 2
+        h ) H=true
             ;;
         l ) L=true
             ;;
@@ -28,7 +28,14 @@ done
 shift $((OPTIND -1))
 [ -z "$1" ] && echo "Usage: markd [OPTIONS] [MARKNAME]" && exit 1
 [ -n "$2" ] && echo "Usage: markd [markname] (optional)[path]" && echo $2 && exit 1
-
+[ $H ] && echo "Usage: markd [OPTIONS] [MARKNAME]" &&
+    echo "[MARKNAME] is whatever character sequence you want to use for the alias" &&
+    echo "e.g. [MARKNAME] = test, will result in the alias 'cd-test'" &&
+    echo "[OPTIONS]:" && echo "-c : will add clear to the alias" &&
+    echo "-l : will add ls to the alias (after clear if -c is used)" &&
+    echo "-p [PATH] : specify which directory to mark, default is current working directory" &&
+    echo "-v : will output the alias that is created" &&
+    echo "Other options to be added"
 tab='    '
 str="cd-$1=\"cd $dir"
 [ $C ] && str="${str} && clear"
